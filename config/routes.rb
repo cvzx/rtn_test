9562ea3c5582dddef7bf4531865fe2d6ab07a5ff
Aleh Shapo <xqvcaa@gmail.com>
2020-02-22 11:11:31 +0300

    Add request spec for movies endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :products do
+    get '/movies', to: 'movies#index'
+  end
 end
---
+++ spec/requests/movies_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Movies list", type: :request do
+  describe "GET /products/movies" do
+    let!(:movies)         { create_list(:movie, 5) }
+    let(:expected_movies) { movies.as_json(only: %i[id, title, plot]) }
+
+    it 'returns list of movies' do
+      get '/products/movies'
+
+      expect(response).to have_http_status(200)
+      expect(response).to match_json_schema('movies/list')
+      expect(response.body).to include_json(expected_items)
+    end
+  end
+end
