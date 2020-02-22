2ade7025283bdf091477c2454d063d950cac3c2c
Aleh Shapo <xqvcaa@gmail.com>
2020-02-22 11:17:40 +0300

    Fix linter issues

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Movies list", type: :request do
-  describe "GET /products/movies" do
+RSpec.describe 'Movies list', type: :request do
+  describe 'GET /products/movies' do
     let!(:movies)         { create_list(:movie, 5) }
-    let(:expected_movies) { movies.as_json(only: %i[id, title, plot]) }
+    let(:expected_movies) { movies.as_json(only: %i[id title plot]) }
 
     it 'returns list of movies' do
       get '/products/movies'
