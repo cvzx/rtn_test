6d5acd360822f4569dab653b017faeb145e65437
Aleh Shapo <xqvcaa@gmail.com>
2020-02-24 16:30:12 +0300

    Add waiting for db running to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+# wait for the postgres docker to be running
+while ! pg_isready -h db -p 5432 -q -U postgres; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
 bin/rails db:reset
 bin/rails server --port 3000 --binding 0.0.0.0
